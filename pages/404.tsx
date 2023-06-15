fb794260da3d441201d3f5d24294db849e782b3a
egor antrop <mougraaa@gmail.com>
2023-06-15 21:40:32 +0400

    gh-pages

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 function NotFound() {
-  return <div className='other'>This page doesn't exist</div>
+  return <div>This page does not exist</div>
 }
 
 export default NotFound
